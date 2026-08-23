ecb24221ddfc68b23aecaec9df57de34e09734b7
Guruprasad594 <guruprasadshetty9060@gmail.com>
2026-08-23 16:54:56 +0530

    Added new files

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sur-shell-v5';
+const CACHE = 'sur-shell-v6';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
