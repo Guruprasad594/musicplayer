7710b2b00316107e47022027ca157f76deb0a07a
Guruprasad594 <guruprasadshetty9060@gmail.com>
2026-09-06 11:53:55 +0530

    queue

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sur-shell-v15';
+const CACHE = 'sur-shell-v17';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
