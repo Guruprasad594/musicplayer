cb5fe53b11f915683d35374c842af770e4c02547
Guruprasad594 <guruprasadshetty9060@gmail.com>
2026-09-06 12:50:13 +0530

    sinup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sur-shell-v22';
+const CACHE = 'sur-shell-v24';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
